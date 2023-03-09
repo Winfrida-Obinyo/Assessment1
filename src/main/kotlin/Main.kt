262fbd0a18cbe6db8cce5afa8eaab2b065be061b
Winfrida-Obinyo <obinyowinfridaa@gmail.com>
2023-03-09 08:50:28 +0300

    assessment1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+fun main(){
+    sum(5,5)
+    substaract(35,10)
+    division(40,2)
+    multiply(30,9)
+
+
+
+
+}
+fun cities(name1:String,name2: String){
+    var names = ("name1","name2")
+    return(names)
+}
+fun numbers(num1:Int,num2:Int,num3:Int):Int{
+    var number = arrayOf(num1,num2,num3,)
+    return (numbers)
+
+
+}
+fun details(name1: String, name2: String,name3:String name4:String):{
+    var names= arrayOf("name1,name2,name3,name4")
+    return (names)
+
+}
+fun sum (num1:Int,num2:Int){
+    var add = (num1+ num2)
+    println(add)
+}
+fun substaract(num1: Int,num2:Int){
+    var sub = (num1-num2)
+    println(sub)
+
+}
+fun division(num1:Int, num2:Int){
+    var divide=(num1/num2)
+    println(divide)
+}
+fun multiply(num1:Int,num2:Int){
+    var mult=(num1*num2)
+    print(mult)
+}
+
+
